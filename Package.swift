2b7dff199bef9229ffe7a8ac7e0faf080f14b6e7
Vatsal Manot <vatsal.manot@yahoo.com>
2026-08-04 05:58:09 +0530

    Updated for release 1.3.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     targets: [
         .target(
             name: "_AtomicsShims_Aggregation",
-            dependencies: ["_AtomicsShims"]
+            dependencies: ["_AtomicsShims"],
+            swiftSettings: [.define("SCIPIO_PRECOMPILED_BINARY_WRAPPER")]
         ),
         .binaryTarget(
             name: "_AtomicsShims",
@@ -23,12 +24,13 @@ let package = Package(
         ),
         .target(
             name: "Atomics_Aggregation",
-            dependencies: ["Atomics", "_AtomicsShims_Aggregation"]
+            dependencies: ["Atomics", "_AtomicsShims_Aggregation"],
+            swiftSettings: [.define("SCIPIO_PRECOMPILED_BINARY_WRAPPER")]
         ),
         .binaryTarget(
             name: "Atomics",
-            url: "https://github.com/swift-precompiled/swift-atomics/releases/download/1.3.1/Atomics-ce0c2f5753588a7e613289d94f20f457db8383c8c1f9c067ebb3a26ca77dd17f.xcframework.zip",
-            checksum: "ce0c2f5753588a7e613289d94f20f457db8383c8c1f9c067ebb3a26ca77dd17f"
+            url: "https://github.com/swift-precompiled/swift-atomics/releases/download/1.3.1/Atomics-c363baba468e29cdcbbb93c3720f5393bfdc9f28e3dba1481b8b4627acb2234a.xcframework.zip",
+            checksum: "c363baba468e29cdcbbb93c3720f5393bfdc9f28e3dba1481b8b4627acb2234a"
         )
     ]
 )
